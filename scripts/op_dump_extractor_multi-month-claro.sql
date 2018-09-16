98029e6bf8bd25b8f6b4e2d97ca15078b35e53c9
Stephanie Buser <sbuser@qti.qualcomm.com>
2018-09-16 16:40:58 -0700

    updating core

--- scripts/op_dump_extractor_multi-month-claro.sql
+++ scripts/op_dump_extractor_multi-month-claro.sql
@@ -1,32 +1,4 @@
 --NetVision data extract for DIRBS
---
---
--- Copyright (c) 2018 Qualcomm Technologies, Inc.
---
--- All rights reserved.
---
--- Redistribution and use in source and binary forms, with or without modification,
--- are permitted (subject to the limitations in the disclaimer below) provided that the
--- following conditions are met:
---
---  * Redistributions of source code must retain the above copyright notice, this list of conditions
---    and the following disclaimer.
---  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions
---    and the following disclaimer in the documentation and/or other materials provided with the distribution.
---  * Neither the name of Qualcomm Technologies, Inc. nor the names of its contributors may be used to endorse
---    or promote products derived from this software without specific prior written permission.
---
--- NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED
--- BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
--- TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
--- THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
--- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
--- DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
--- STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
--- EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
---
---
---
 --Reuse CRC data to generate dummy operator input dump for DIRBS
 --This version takes all the 5 months data for one operator = Claro, mapping to Mobilink
 --Noite this operator has separate voice and data 
@@ -69,7 +41,35 @@
 --NV_VARIABLE_DECLARE_VARIABLE = $mylimit,string,100
 --NV_VARIABLE_DECLARE_VARIABLE = $myunion,string,all
 --NV_VARIABLE_DECLARE_VARIABLE = $kill,string,--
---NV_VARIABLE_DECLARE_END 
+--NV_VARIABLE_DECLARE_END
+-- Copyright (c) 2018 Qualcomm Technologies, Inc.
+--
+--  All rights reserved.
+--
+--
+--
+--  Redistribution and use in source and binary forms, with or without modification, are permitted (subject to the
+--  limitations in the disclaimer below) provided that the following conditions are met:
+--
+--
+--  * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+--  disclaimer.
+--
+--  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+--  disclaimer in the documentation and/or other materials provided with the distribution.
+--
+--  * Neither the name of Qualcomm Technologies, Inc. nor the names of its contributors may be used to endorse or promote
+--  products derived from this software without specific prior written permission.
+--
+--  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY
+--  THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+--  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+--  COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+--  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+--  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+--  TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+--  POSSIBILITY OF SUCH DAMAGE.
+--
 
 --log outputs table
 create temp table loggy (
